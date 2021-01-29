40216d5b6d805f4ae85129997de50f07783e1c90
Emily Chiu <emily.cindy.chiu@gmail.com>
2021-01-28 17:40:29 -0800

    exported state, checkIn, makeAMove, and reset

--- src/state.ts
+++ src/state.ts
@@ -1,46 +1,55 @@
-import React, { Component } from 'react',
+import React, { Component } from 'react';
 import axios from 'axios';
+import { updateStatement } from 'typescript';
+import { ServerState } from '../types/index'
 
-const intialState: State = {
+export const state: State = {
     gameId: 0,
     myColor: 'red',
     myTurn: false,
     moves: []
 }
 
+type Color = 'red' | 'black'
+
 type State = {
     gameId: number
-    myColor: 'red' | 'black'
+    myColor: Color
     myTurn: boolean
     moves: []
 }
 
-const checkIn = (state: State) => {
-    // uses axios to post {gameId: number, color: 'red' | black'} to the server's /check-in endpoint
-    axios.post('/check-in', (req, res) => {
-        res.send(state.gameId, state.myColor)
-    })
+let timer
 
+export const checkIn = async (gameId: number, myColor: Color) => {
+    // uses axios to post {gameId: number, color: 'red' | black'} to the server's /check-in endpoint
+    const currentState: ServerState = await axios.post('/check-in', { gameId, myColor })
+    if (currentState.whoseTurn === state.myColor) {
+        state.myTurn = true
+        clearInterval(timer)
+        alert('It\'s my turn!')
+    }
 }
 
-const makeAMove = ({gameId, x, y, color}, myTurn) => {
-    // if (myTurn === false) {
-       
-    //     //    do nothing?
-    // }
-    if (myTurn === true) {
-        axios.post('/make-a-move', (req, res) => {
-            // if myTurn = true, use the axios npm package to send {x, y, color} to /make-a-move endpoint
-            // or is this in the bigger scope, and myTurn if statements are inside?
-            res.send({ x, y, color })
-        })
-        
-    } 
+
+export const makeAMove = (gameId: number, x: number, y: number, color: Color) => {
+    if (state.myTurn === true) {
+        axios.post('/make-a-move', { x, y, color })
     }
     // starts a setInterval, in which checkIn is called.
-    setInterval(checkIn, 3000)
+    timer = setInterval(() => {
+        checkIn(state.gameId, state.myColor)
+    }, 3000)
 }
 
-const reset = () => {
+
+export const reset = (state) => {
     // resets state to initial state
+    state = {
+        gameId: 0,
+        myColor: 'red',
+        myTurn: false,
+        moves: []
+    }
+    return state
 }
