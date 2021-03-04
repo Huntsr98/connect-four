a6e983bab2f965985c67a4b5de361bb300b3d83d
Emily Chiu <emily.cindy.chiu@gmail.com>
2021-03-04 10:15:31 -0800

    set up axiosMock stuff

--- src/state.ts
+++ src/state.ts
@@ -4,14 +4,14 @@ import { ServerState, Move } from '../types/index'
 
 export const state: State = {
     gameId: 0,
-    myColor: 'red',
+    myColor: 'black',
     myTurn: true,
     moves: [
-            {
-                color: 'red',
-                x: 3,
-                y: 0
-            }
+        {
+            color: 'red',
+            x: 3,
+            y: 0
+        }
     ]
 }
 
@@ -25,42 +25,60 @@ type State = {
 }
 
 let timer
-const axiosMock = (): ServerState => {
-    return {
-        moves: [], 
-        scores: {
-            client1: 0,
-            client2: 0
-        },
-        boardDimension: {
-            x: 7,
-            y: 8
-        },
-        whoseTurn: 'red',
-        gameId: 1
-    }
-}
-export const checkIn = async (gameId: number, myColor: Color) => {
+// const axiosMock = (): ServerState => {
+//     return {
+//         moves: [],
+//         scores: {
+//             client1: 0,
+//             client2: 0
+//         },
+//         boardDimension: {
+//             x: 7,
+//             y: 8
+//         },
+//         whoseTurn: 'red',
+//         gameId: 1
+//     }
+// }
+export const checkIn = (gameId: number, myColor: Color) => {
     // uses axios to post {gameId: number, color: 'red' | black'} to the server's /check-in endpoint
     // const currentState: ServerState = await axios.post('http://localhost:3000/check-in', { gameId, myColor })
-    const currentState: ServerState = await axiosMock()
+    
+    // HW: replace axiosMock() with a real call to the server for state
+    // use /check-in
+    // deal with the promise that axios.post returns, which will trigger state.myTurn = true
+    const currentState: ServerState = axiosMock()
     if (currentState.whoseTurn === state.myColor) {
         state.myTurn = true
         clearInterval(timer)
         alert('It\'s my turn!')
+
     }
 }
 
 
 export const makeAMove = (gameId: number, x: number, y: number, color: Color) => {
     if (state.myTurn === true) {
-        axios.post('http://localhost:3000/make-a-move', { x, y, color })
+        const responsePromise = axios.post('http://localhost:3000/make-a-move', { x, y, color })
+        
+        // response looks like 
+        //{
+        //     message: 'okay :)',
+        //     moves: state.moves
+        // }
+        debugger
+        responsePromise.then(({data}) => {
+            // starts a setInterval, in which checkIn is called.
+            debugger
+            timer = setInterval(() => {
+                checkIn(state.gameId, state.myColor)
+            }, 3000)
+            state.moves = data.moves
+        })
     }
-    // starts a setInterval, in which checkIn is called.
-    timer = setInterval(() => {
-        checkIn(state.gameId, state.myColor)
-    }, 3000)
+
 }
+
 
 
 export const reset = (state) => {
