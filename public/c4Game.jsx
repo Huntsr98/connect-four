d583bbc404aba1014acd59cd461cb5c3d91b294a
Steven Hunt <huntsr98@stevenhsmacbook.home>
2021-03-04 20:14:54 -0500

    converted to react and started to revise data structure

--- public/c4Game.jsx
+++ public/c4Game.jsx
@@ -8,7 +8,7 @@
 
 import * as stateFunctions from '../src/state'
 import React, { useState } from 'react'
-import ReactDOM from 'react-dom' 
+import ReactDOM from 'react-dom'
 
 
 //this is temporary stub
@@ -17,13 +17,14 @@ stateFunctions.makeAMove(stateFunctions.state.gameId, stateFunctions.state.moves
 const config = {
     rowHeight: 300,
     rowWidth: 300,
-    boardWidth: 1000,
-    boardHeight: 1000,
-    gamePieceDiameter: 100
+    boardWidth: 750,
+    boardHeight: 750,
+    gamePieceDiameter: 75,
+    boarderRadius: 100
 }
 
 
-const Board = ({rows}) => {
+const Board = ({ rows }) => {
     const boardStyle = {
         width: config.boardWidth + 'px',
         height: config.boardHeight + 'px'
@@ -40,33 +41,62 @@ const Row = ({ pieceArray }) => {
         width: '100%',
         display: 'align-items'
     }
-    const gamePieces = pieceArray.map((gamePiece) => {
-        return <GamePiece color={gamePiece}></GamePiece>
+
+    // 1. What type is clickChoice?  A function
+    // 2. When do you want the desired event to happen? What should trigger it?  when a user clicks a gamepiece.
+    // 3. When the event is triggered, what exactly do you want to happen? makeAMove should fire with the users choice as the input
+
+    const gamePieces = pieceArray.map((gamePiece, whichPiece) => {
+        return <GamePiece color={gamePiece} key={whichPiece}></GamePiece> , whichPiece
+        // I want to see if "whichPiece" = the piece that was clicked.  If it is, the I want "makeAMove" 
+        // to be called, feeding it with the piece that was clicked
+
+
     })
 
-    return <div style={rowStyle} className="row">
+    return <div style={rowStyle} className="row" >
         {gamePieces}
     </div>
 }
 
-const GamePiece = ({color}) => {
+const GamePiece = ({ color }) => {
     const style = {
         width: config.gamePieceDiameter + 'px',
         height: config.gamePieceDiameter + 'px',
         background: color,
         flexDirection: 'center',
+        borderRadius: config.boarderRadius + 'px'
     }
-    return <div style={style} classname="gamePiece">
+    debugger
+    const clickChoice = (gamePieces) => {
+        const key = gamePieces.key
 
+        if (key === clickChoice) {
+            let choice = key
+            makeAMove(choice)
+        }
+    }
+    return <div style={style} className="gamePiece" >
     </div>
+  
+
 }
 const arg = [
-    ['red', 'black', null, 'red', null, null, null],
-    ['black', 'black', 'red', 'black', 'red', null, 'black'],
-    ['black', 'red', 'black', 'black', 'black', null, 'black'],
-    ['black', 'red', 'red', 'red', 'black', 'red', 'red'],
-    ['red', 'black', 'red', 'black', 'black', 'red', 'black'],
-    ['black', 'red', 'black', 'red', 'red', 'black', 'black'],
+    ['red', ],
+    ['black'],
+    ['black', 'red'],
+    [],
+    ['red', 'black', 'red'],
+    ['black', 'black'],
+    []
 ]
+const column = arg[2]
+const piece = column[1]
+
 
 ReactDOM.render(<Board rows={arg}></Board>, document.getElementById('root')); //putting 'element' inside of 'root' element.  
+// HW 2/18
+// when the user clicks on a column of the board, we want makeAMove to fire with the appropriate arguements 
+// hint:  when creating the divs, some will get "on click" events 
+// search how to add ON CLICK to a div, specifically a gamepiece?
+//
